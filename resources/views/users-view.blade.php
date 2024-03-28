efb48443365680bc8d5d6d4d478242f525a77a62
Maximilian Huber <maximilian.huber@odt.net>
2024-03-28 16:22:11 +0100

    button size user view

--- resources/views/users-view.blade.php
+++ resources/views/users-view.blade.php
@@ -26,7 +26,7 @@
                             <td class="px-4 py-2">
                                 {{$aUser->name}}
                             </td>
-                            <td class="px-4 py-2 text-right whitespace-nowrap">
+                            <td class="px-4 py-2 text-right whitespace-nowrap pb-4">
                                 <x-button type="" text="Add"
                                           class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"/>
                             </td>
