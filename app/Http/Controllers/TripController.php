09e11c9fdc78526813a108f9029b6557d99c01ca
Maximilian Huber <maximilian.huber@odt.net>
2024-03-29 17:08:11 +0100

    assing creator

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -6,6 +6,8 @@ use App\Http\Requests\TripPostRequest;
 use App\Models\Trip;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class TripController extends Controller
 {
@@ -40,7 +42,14 @@ class TripController extends Controller
             $validated['duration_in_days'] = $carbonDate1->diffInDays($carbonDate2) + 1;
         }
 
-        Trip::create($validated->toArray());
+        $trip = Trip::create($validated->toArray());
+        
+        DB::table('user_has_trip')->insert([
+            'user_id' => Auth::id(),
+            'trip_id' => $trip->id,
+            'type' => 'owner',
+            'status' => 'waiting'
+        ]);
 
         return redirect('/trips')->with('success', 'Trip created successfully!');
     }
